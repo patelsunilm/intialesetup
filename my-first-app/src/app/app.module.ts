e967aef6c17693f396b2af27722b0c048b8af32c
patelsunilm <patelsunil202@gmail.com>
2017-09-27 16:44:35 +0530

    routing

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MemberComponent } from './member/member.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    MemberComponent,
+    HeaderComponent,
+    UserComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'member',component: MemberComponent},
+      {path: 'product', component: ProductComponent},
+      {path: 'home', component: HomeComponent},
+      {path: 'user', component: UserComponent},
+      {path: 'login', component: LoginComponent},
+      //{path: '**', }
+  ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
