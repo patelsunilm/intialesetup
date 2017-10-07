f48ee6a8f99e343da6c269ecd6247091b9fd3f20
patelsunilm <patelsunil202@gmail.com>
2017-10-07 10:36:28 +0530

    pipes

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,36 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { RouterModule } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { MemberComponent } from './member/member.component';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-
+import { ShortenPipe } from './shorten.pipe';
+import { FillterPipe } from './fillter.pipe';
+ 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    MemberComponent,
-    HeaderComponent,
-    UserComponent,
-    HomeComponent,
-    LoginComponent
+    ShortenPipe,
+    FillterPipe
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {path: 'member',component: MemberComponent},
-      {path: 'product', component: ProductComponent},
-      {path: 'home', component: HomeComponent},
-      {path: 'user', component: UserComponent},
-      {path: 'login', component: LoginComponent},
-      //{path: '**', }
-  ])
-    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
